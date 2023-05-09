ab8d357715b93dfcf0d3a8f6c6cbc27ad198bae6
Mankalas1 <132981734+Mankalas1@users.noreply.github.com>
2023-05-09 06:23:13 +0000

    added code base to Develop branch
    added post model
    
    added Create Post route

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, jsonify, request
+from Models.posts import posts,Post
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return jsonify({"status": "Welcome!"})
+
+
+@app.route('/posts', methods=['GET'])
+def hello():
+    return jsonify({"posts": posts})
+@app.route('/post', methods=['POST', 'GET'])
+def add_post():
+    post = Post(request.json["title"], request.json["description"])
+    posts.append(post)
+    return jsonify(post.to_json()), 201
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
---
+++ models/posts.py
@@ -0,0 +1,25 @@
+posts = {
+    1: {
+        "title": "Hello World!",
+        "Description": "This is my first post!"
+    },
+    2: {
+        "title": "San Francisco",
+        "Description": "I love this city!"
+    },
+    3: {"title": "New York",
+        "Description": "I love this city too!"
+        }
+}
+class Post():
+    def __init__(self, title, content):
+        self.title = title
+        self.description = content
+    def to_json(self):
+        return {
+            "title": self.title,
+            "description": self.description
+        }
+    def from_json(self, json):
+        self.title = json["title"]
+        self.description = json["description"]
